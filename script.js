2152255b42e0ef01552576d8b105668792024a7b
George Cheng <sparklyspuds@gmail.com>
2022-01-10 00:46:15 +0800

    JS, CSS, HTML update

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-// Getting elements
-
+// Getting elements -Page 1.
 var start_quiz_btn = document.querySelector(".start-btn button");
 var info_box = document.querySelector(".info-box");
+
+// Getting elements -Page 2.
 var quit_btn = info_box.querySelector(".info-button .quit");
 var continue_btn = info_box.querySelector(".info-button .restart");
 var quiz_box = document.querySelector(".quiz-box");
-var option_list = document.querySelector(".option-list");
+
+// Getting elements -Page 3.
 var timer_read = quiz_box.querySelector(".sec");
+var option_list = document.querySelector(".option-list");
+var next_btn = document.querySelector(".next-btn");
+var new_score = document.querySelector(".score");
+var question_text = document.querySelector(".quiz-text");
+// elements to inform if answer was correct
+var tick_Icon = '<div class="icon-tick"><i class="far fa-check-circle"></i></div>';
+var cross_Icon = '<div class="icon-cross"><i class="far fa-times-circle"></i></div>';
 
-// First page- Start page
+// Getting elements -Page 4.
+var result_box = document.querySelector(".result-box");
+var score_btn = result_box.querySelector(".quit");
+var username = document.getElementById("input");
+var replay_btn = result_box.querySelector(".restart");
+
+// Page 1.- Start page
+
 // Start quiz button click event
-
 start_quiz_btn.addEventListener("click", function() {
     info_box.classList.add("clicked"); //show the info box
 });
 
-// 2nd page- Info page
-// Quit + Continue button click event
+// Page 2.- Info page
 
+// Quit + Continue button click event
 quit_btn.addEventListener("click", function() {
     info_box.classList.remove("clicked"); //hide the info box
 });
@@ -29,31 +44,31 @@ continue_btn.addEventListener("click", function() {
     timerStart(); //start the timer
 });
 
-// 3rd page- Question page
+//Page 3.- Question page
 
+// start question 1. with 120 secs
 var question_count = 0;
 var time_remain = 120;
-var next_btn = document.querySelector(".next-btn");
-var new_score = document.querySelector(".score")
 
-// Timer event
-
+// endgame funtion
 function endgame() {
 
     // record final score
     new_score = time_remain;
 
+    // reset timer back to 120 secs
     clearInterval(timer_count);
-
-    // reset timer 
     time_remain = 120;
     timer_read.textContent = time_remain;
     return;
 }
 
+// timer
 function timerStart() {
     timer_count = setInterval(function() {
+        // start the timer from 119.99s instead of 120.99s
         time_remain = time_remain - 1;
+        // stop timer when timer goes to 0
         if (time_remain < 0) {
             endgame();
         } else {
@@ -63,9 +78,9 @@ function timerStart() {
 }
 
 // Next button click event
-
 next_btn.addEventListener("click", function() {
 
+    // show next question if its not the last page
     if (question_count < questions.length - 1) {
         question_count++;
         showQuestion(question_count);
@@ -73,6 +88,7 @@ next_btn.addEventListener("click", function() {
         // only shows button if option was selected
         next_btn.style.display = "none";
 
+        // show Page 4. if it's the end of the quiz
     } else {
         quiz_box.classList.remove("clicked"); //hide the quiz box
         result_box.classList.add("clicked"); //show the result box
@@ -82,50 +98,48 @@ next_btn.addEventListener("click", function() {
     }
 });
 
-
-
-// Getting question
-
-function showQuestion(index) {
-    var question_text = document.querySelector(".quiz-text");
-    var question_tag = '<span>' + questions[index].question + '</span>';
+// Function that gets displays questions
+function showQuestion(question_count) {
+    // element that creates question text from question.js-"question" array
+    var question_tag = '<span>' + questions[question_count].question + '</span>';
+    // element that creates option text responsive to the question
     var option_tag =
-        '<div class="option">' + questions[index].options[0] + '<span></span></div>' +
-        '<div class="option">' + questions[index].options[1] + '<span></span></div>' +
-        '<div class="option">' + questions[index].options[2] + '<span></span></div>' +
-        '<div class="option">' + questions[index].options[3] + '<span></span></div>';
+        '<div class="option">' + questions[question_count].options[0] + '</div>' +
+        '<div class="option">' + questions[question_count].options[1] + '</div>' +
+        '<div class="option">' + questions[question_count].options[2] + '</div>' +
+        '<div class="option">' + questions[question_count].options[3] + '</div>';
 
     question_text.innerHTML = question_tag;
     option_list.innerHTML = option_tag;
 
-    // when answer was picked
+    // array that selected all option div
     var option = option_list.querySelectorAll(".option");
+
+    // when option was clicked ,check if its correct
     for (var i = 0; i < option.length; i++) {
         option[i].setAttribute("onclick", "optionSelected(this)");
     }
 }
 
-// Check if answer was correct with responsive color
-var tick_Icon = '<div class="icon-tick"><i class="far fa-check-circle"></i></div>';
-var cross_Icon = '<div class="icon-cross"><i class="far fa-times-circle"></i></div>';
-
-
+//Function that checks option selected
 function optionSelected(answer) {
+    // elements that represent the option selected
     var chosen_answer = answer.textContent;
+    // elements that represent the correct answer
     var correct_answer = questions[question_count].answer;
-
+    // if option selected is correct
     if (chosen_answer == correct_answer) {
+        // inform with responsive css class + tick icon at the end
         answer.classList.add("correct");
         answer.insertAdjacentHTML('beforeend', tick_Icon);
     } else {
+        // inform with responsive css class + cross icon at the end
         answer.classList.add("incorrect");
         answer.insertAdjacentHTML('beforeend', cross_Icon);
-
         // subtract time from providing wrong answer
         time_remain = time_remain - 20;
     }
-
-    // Disable options once answer was selected
+    // Disable options once answer was selected avoid muti-selected
     var disable_opt = option_list.children.length;
     for (let i = 0; i < disable_opt; i++) {
         option_list.children[i].classList.add("disabled");
@@ -134,32 +148,91 @@ function optionSelected(answer) {
     next_btn.style.display = "block";
 }
 
-// 4th page Result page
-
-var result_box = document.querySelector(".result-box");
-var score_btn = result_box.querySelector(".quit");
-var user_name = document.getElementById("input");
-
-var replay_btn = result_box.querySelector(".restart");
+// Page.4 Result page
 
 // replay button event
-
-// replay_btn.addEventListener("click", function() {
-//     result_box.classList.remove("clicked"); //hide the quiz box
-// });
-
-// Score button event
-var leaderboard = [];
 var scoreboard_box = document.querySelector(".scoreboard-box");
+var scoreboard_content = document.querySelector(".scoreboard");
 
+// make a array stores the scores
+var leaderboard = [];
+
+// function assists sort scores 
+function compareScores(a, b) {
+    return b.leaderboard - a.leaderboard;
+}
+
+// funtion displays score 
+function displayScores() {
+    // reset scoreboard content
+    scoreboard_content.innerHTML = "";
+    //sort scores in decending order  
+    leaderboard.sort(compareScores);
+    // create a loop adding content to the scoreboard
+    for (var i = 0; i < leaderboard.length; i++) {
+        var li = document.createElement("li");
+        li.textContent = leaderboard[i].name + " - " + leaderboard[i].score + " points.";
+        scoreboard_content.appendChild(li);
+    }
+}
+
+// funtion stores scores in JSON string
+function storeScore() {
+    localStorage.setItem("leaderboard", JSON.stringify(leaderboard));
+}
+
+// function loads scores
+function loadScores() {
+    var memory = JSON.stringify(leaderboard);
+    if (memory) {
+        leaderboard = memory
+    }
+}
+
+// Scoreboard button event
 score_btn.addEventListener("click", function(event) {
     event.preventDefault();
+    //grab username entered and trim redundent 
+    var username_entered = username.value.trim();
+
+    //inform if form is empty
+    if (!username_entered) {
+        alert("Please Enter your Name");
+        return;
+    }
+    // object with resultscore and username
     var resultscore = {
-        name: user_name.value.trim(),
+        name: username_entered,
         score: new_score,
     }
-    console.log(resultscore);
-    localStorage.setItem("resultscore", JSON.stringify(resultscore));
+    leaderboard.push(resultscore); // push the objct into leaderboard array
+    // empty the input
+    username.value = "";
+    // save & display
+    storeScore();
+    displayScores();
     result_box.classList.remove("clicked"); //hide the info box
     scoreboard_box.classList.add("clicked"); // show scoreboard
 });
+
+// Page.5 scoreboard
+
+var replay_quiz = scoreboard_box.querySelector(".restart ");
+var clear_scores = scoreboard_box.querySelector(".clear");
+
+// Replay button event
+replay_quiz.addEventListener("click", function() {
+    clearInterval(timer_count);
+    scoreboard_box.classList.remove("clicked"); // hide the scoreboard box
+    // reset question count
+    question_count = 0;
+})
+
+// Clear button event
+clear_scores.addEventListener("click", function() {
+    // wipe local storage
+    localStorage.clear();
+    // reset leaderboard array then display
+    leaderboard = [];
+    displayScores();
+})
